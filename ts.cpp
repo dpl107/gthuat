f473814d7e0a77d9548b26867e06604da3fee5f5
dpl107 <hedgehog7421@gmail.com>
2025-04-23 14:48:05 +0700

    test

--- ts.cpp
+++ ts.cpp
@@ -1,36 +1,35 @@
-#include <bits/stdc++.h>
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
 using namespace std;
-# define ll long long
-# define st string
-# define endl "\n"
-# define fast ios_base::sync_with_stdio(false); cin.tie(nullptr)
-void print(vector<ll>&a){
-    for(auto x:a)cout<<x<<" ";
-    cout<<"\n";
+#define pb push_back
+#define fast ios_base::sync_with_stdio(false);cin.tie(nullptr)
+void xuly(int x,vector<int> &a,int n){
+    for(int i=0;i<n;i++){
+        if(a[i]==x){
+            a[i]=0;
+        }
+    }
 }
+const int maxn = 1e6;
+int d[maxn];
 int main(){
     fast;
-    clock_t st,ed;
-    st = clock();
-    
-    ll n;cin>>n;
-    vector<ll> a(n);
-    for(ll i=0;i<n;i++)cin>>a[i];
-
-    ll index,newnum;
-    for(ll i=0;i<n;i++){
-        index=i;
-        newnum=a[i];
-        while(index>0&&a[index-1]>newnum){
-            a[index]=a[index-1];
-            index--;
-        }
-        a[index]=newnum;
-        cout<<"Buoc "<<i+1<<": ";
-        print(a);
+    int n,m;
+    cin>>n>>m;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        d[a[i]]++;
     }
-    ed = clock();
-    ll tt = double(ed-st)/double(CLOCKS_PER_SEC);
-}       
+    for(int i=0;i<m;i++){
+        int x;cin>>x;
+        xuly(x,a,n);
+    }
+    int res = -1e6;
+    for(auto x:a){
+        if(d[x]>=1){
+            res = max(res,x*d[x]);
+            d[x]=0;
+        }
+    }
+    cout<<res;
+}
